093f4d788ac8a73c43562262a4da9fdba0ac021c
Andrew <iglooworks1505@gmail.com>
2021-03-25 13:05:46 -0500

    Some minor style tweaks and adjusted some text. Committing before I break everything trying to alternate alignment and animations in the portfolio section

--- client/src/components/PortfolioSection.js
+++ client/src/components/PortfolioSection.js
@@ -1,28 +1,64 @@
-import React from 'react'
-import {portfolioArray, HeroImagePallet} from '../UniversalData'
-import portfolioImageOne from '../assets/portfolioImageOne.png'
+import React from "react";
+import { portfolioArray, HeroImagePallet } from "../UniversalData";
+import portfolioImageOne from "../assets/portfolioImageOne.png";
+import triviaScreenShot from "../assets/triviaScreenShot.png";
+import jeopardyScreenShot from "../assets/jeopardyScreenShot.png";
 
 const PortfolioSection = () => {
-    const imageArray = [
-        portfolioImageOne
-    ]
-    return (
-        <section className="portfolio">
-        {portfolioArray.map(p => (
-            <div className={`portfolio-container transitionLeft${p.transitionIndex}`}>
-            <div className={`portfolio-${p.orientation}-${p.transitionIndex}`}>
+  const imageArray = [triviaScreenShot, jeopardyScreenShot];
+  return (
+    <section className="portfolio">
+      {portfolioArray.map((p) => (
+        <div
+          className={`portfolio-container transitionLeft${p.transitionIndex}`}
+        >
+          <div className={`portfolio-${p.orientation}-${p.transitionIndex}`}>
             <div className="inner">
-            <p className="subtitle" style={{color: HeroImagePallet.secondary}}>{p.appName}</p>
-            <p className="featured-title" >{p.subTitle}</p>
-            <p className="featured-desc">{p.description}</p>
+              <p
+                className="subtitle"
+                style={{ color: HeroImagePallet.secondary }}
+              >
+                {p.appName}
+              </p>
+              <p className="featured-title">{p.subTitle}</p>
+              <p className="featured-desc">{p.description}</p>
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <p style={{ marginRight: "50px" }}>
+                  <a
+                    href={p.repo}
+                    style={{
+                      textDecoration: "none",
+                      color: "rgb(0, 160, 242)",
+                    }}
+                  >
+                    Repo
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href={p.url}
+                    style={{
+                      textDecoration: "none",
+                      color: "rgb(0, 160, 242)",
+                    }}
+                  >
+                    Live Project
+                  </a>
+                </p>
+              </div>
             </div>
-            </div>
-            <a href="#!"><img className={`portfolio-right transitionRight${p.transitionIndex}`}src={imageArray[p.index]} alt="Portfolio Image"/></a>            
-            </div>
-            ))
-        }
-        </section>
-    )
-}
+          </div>
+          <a href={p.url}>
+            <img
+              className={`portfolio-right transitionRight${p.transitionIndex}`}
+              src={imageArray[p.index]}
+              alt="Portfolio"
+            />
+          </a>
+        </div>
+      ))}
+    </section>
+  );
+};
 
-export default PortfolioSection
+export default PortfolioSection;
